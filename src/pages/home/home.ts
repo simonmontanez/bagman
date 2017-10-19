d98dabf6240e1a6ea8ed0ee9f41adaacddd6210d
Simón <simonmontanez@seven4n.com>
2017-10-19 10:19:59 -0500

    Live check

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {LiveReply, LiveRequest} from "../../app/proto/underwood_pb";
+import {grpc, BrowserHeaders, Code} from "grpc-web-client";
+import {LiveChecker} from "../../app/proto/underwood_pb_service";
 
 @Component({
   selector: 'page-home',
@@ -7,7 +10,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  host: string = 'http://localhost:8080';
+  liveResponse: string;
+
   constructor(public navCtrl: NavController) {
+    //this.liveTest();
+  }
+
+  liveTest() {
+    const request = new LiveRequest();
+
+    grpc.invoke(LiveChecker.Live,{
+      host: this.host,
+      request: request,
+
+      onMessage:(message:LiveReply) => {
+        console.log("live check: ", message.toObject());
+        this.liveResponse = message.getMessage()
+      },
+
+      onEnd:(code:Code, message:string, trailers:BrowserHeaders) => {
+        if (code == Code.OK) {
+          console.log("all ok");
+        } else {
+          console.log("hit an error", code, message, trailers);
+        }
+      }
+    })
 
   }
 
